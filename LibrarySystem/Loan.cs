2adc886a495280f618077e10826aec12053a6db2
James Z <125828382+Jamziewebdev@users.noreply.github.com>
2024-12-06 17:25:44 +0100

    Fullständigt bibliotekssystem
    Denna version lämnade jag in som mitt individuella programmeringsprojekt i grundläggande programmering. Förbättringar är planerade och kommer att komma över tid.

---
+++ LibrarySystem/Loan.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity.Migrations.Model;
+
+namespace LibrarySystem
+{
+    public class Loan
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; } 
+        public required string ISBN { get; set; } 
+        public DateTime LoanDate { get; set; }
+        public DateTime? ReturnDate { get; set; }
+    }
+}
