b0b0f8a6ca48196989c0681b778c8fdc904ce7ef
James Z <125828382+Jamziewebdev@users.noreply.github.com>
2024-12-08 17:25:06 +0100

    Add files via upload

---
+++ C#/LabbSQL/UserContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+namespace sqlLab
+{
+    public class UserContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public UserContext() : base("name=UserDatabaseConnectionString")
+        { 
+
+        }
+    }
+}
+
